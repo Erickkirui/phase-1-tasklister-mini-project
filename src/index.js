422eacaea45a7e3e322388fe423a0651dd3ffb89
Erickkirui <erickkirui653@gmail.com>
2023-03-27 13:18:45 +0300

    created event listener that pick the items from the input and pust them in a list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form")
+  // change the id of the submit input 
+  const a = document.querySelector('input')
+  a.setAttribute('id', 'submit_input')
+  
+  // event listener to the submit
+  
+  form.addEventListener( 'submit', (e) => {
+  
+  e.preventDefault()// return the form to default 
+  // callback function to capture the txt
+  addItemToDoList(e.target.submit_input.value)
+  form.reset()
+  
+  }
+
+  )
+
 });
+
+function addItemToDoList(addedItems){
+  //create the list of items
+  let li = document.createElement('li')
+  li.textContent = addedItems
+
+  document.querySelector("#tasks").appendChild(li)
+
+
+}
+
